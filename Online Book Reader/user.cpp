c171dd25e95c36f236bfa4e3d46a1db738c1de41
Ahmed Salah <elshitehi1@gmail.com>
2024-01-25 01:10:57 +0200

    user

--- Online Book Reader/user.cpp
+++ Online Book Reader/user.cpp
@@ -1,3 +1,70 @@
 #include "user.h"
 
 
+void User::ReadInfo(string &user_name) {
+
+    SetUser_name(user_name);
+    string str;
+
+    cout << "Enter password (no spaces): ";
+    cin >> str;
+    SetPassword(str);
+
+    cout << "Enter first name (no spaces): ";
+    cin >> str;
+    SetFirst_name(str);
+
+    cout << "Enter last name (no spaces): ";
+    cin >> str;
+    SetLast_name(str);
+
+    cout << "Do you want to be admin? (Y/N): ";
+    char c;
+    cin >> c;
+    if (toupper(c) == 'Y') {
+        cout << "Enter admins code (no spaces): ";
+        cin >> str;
+        if (str == "m3rof")
+            Set_admin(true);
+        else
+            Set_admin(false);
+    }
+}
+
+void User::SetUser_name(string &user_name) {
+    this->user_name = user_name;
+}
+
+void User::SetFirst_name(string &first_name) {
+    this->first_name = first_name;
+}
+
+void User::SetLast_name(string &last_name) {
+    this->last_name = last_name;
+}
+
+void User::SetPassword(string &password) {
+    this->password = password;
+}
+
+void User::Set_admin(bool admin) {
+    this->admin = true;
+}
+
+void User::PrintInfo() {
+    cout << "Name: " << first_name << " " << last_name;
+    if (admin)
+        cout << " | Admin";
+    cout << '\n';
+    cout << "User name: " << user_name << '\n';
+}
+
+const string &User::GetUserName() const {
+    return user_name;
+}
+
+const string &User::GetPassword() const {
+    return password;
+}
+
+
--- Online Book Reader/user.h
+++ Online Book Reader/user.h
@@ -16,7 +16,15 @@ private:
     string password;
     bool admin;
 public:
-
+    void ReadInfo(string &user_name);
+    void PrintInfo();
+    void SetUser_name(string &user_name);
+    void SetFirst_name(string &basicString);
+    void SetLast_name(string &basicString);
+    void SetPassword(string &basicString);
+    void Set_admin(bool admin);
+    const string &GetUserName() const;
+    const string &GetPassword() const;
 };
 
 
