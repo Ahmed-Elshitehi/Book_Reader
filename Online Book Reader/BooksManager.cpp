256f1e8e9ef21fb84e5e4c5b2ebb38dfeca3113d
Ahmed Salah <elshitehi1@gmail.com>
2024-01-25 21:31:28 +0200

    last update

--- Online Book Reader/BooksManager.cpp
+++ Online Book Reader/BooksManager.cpp
@@ -21,4 +21,45 @@ void BooksManager::ListAvailableBooks() {
     }
 }
 
+void BooksManager::LoadDatabase() {
+    cout << "BooksManager: LoadDatabase\n";
+    Book* book1 = new Book();
+    book1->SetIsbn("00001");
+    book1->SetAuthor("Ahmed");
+    book1->SetTitle("C++ how to program");
+    vector<string> pages1 = { "A C++", "B C++", "C C++", "D C++", "E C++" };
+    book1->SetPages(pages1);
+    AddBook(book1);
+
+    Book* book2 = new Book();
+    book2->SetIsbn("00002");
+    book2->SetAuthor("Morad");
+    book2->SetTitle("Intro to algo");
+    vector<string> pages2 = {"A Algo", "B Algo", "C Algo", "D Algo", "E "};
+    book2->SetPages(pages2);
+    AddBook(book2);
+    Book* book3 = new Book();
+    book3->SetIsbn("00003");
+    book3->SetAuthor("Morad");
+    book3->SetTitle("Data Structures in C++");
+    vector<string> pages3 = { "A Data", "B Data", "C Data", "D Data", "E Data" };
+    book3->SetPages(pages3);
+    AddBook(book3);
+}
+
+void BooksManager::FreeLoad() {
+    for (auto book: library) {
+        delete book.second;
+    }
+    library.clear();
+}
+
+BooksManager::~BooksManager() {
+    FreeLoad();
+}
+
+map<string, Book *> &BooksManager::GetLibrary() {
+    return library;
+}
+
 
--- Online Book Reader/BooksManager.h
+++ Online Book Reader/BooksManager.h
@@ -7,10 +7,14 @@
 class BooksManager {
 private:
     map<string, Book*> library;
+    void FreeLoad();
 public:
     BooksManager();
     void AddBook(Book *);
     void DeleteBook(string isbn);
     void ListAvailableBooks();
+    void LoadDatabase();
+    map<string, Book *> &GetLibrary();
+    ~BooksManager();
 };
 #endif
